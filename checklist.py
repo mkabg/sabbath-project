0a79e0f627ef1e2914d48d7c91461b24406db034
mkabg <m7110645k@gmail.com>
2025-07-16 21:57:39 +0300

    part 2

--- checklist.py
+++ checklist.py
@@ -1,6 +1,24 @@
-missiolist = ["קניית חלה", "בישול חמין", "הדלקת נרות", "סידור שולחן", "כיבוי טלפון"]
+mission_list = ["קניית חלה", "בישול חמין", "הדלקת נרות", "סידור שולחן", "כיבוי טלפון"]
+
+# part 1
 def numbering(list):
     for index, mission in enumerate(list):
         print(f"{index} {mission}")
 
-numbering(missiolist)
+# part 2
+def add_status(list):
+    status = []
+    for index, mission in enumerate(list):
+        status.append([index, mission, "not done"])
+    return status
+
+def update_status(num, list):
+    for status in list:
+        if status[0] == num:
+            status[2] = "done"
+            print(status)
+            return
+
+# numbering(mission_list)
+# print(add_status(mission_list))
+update_status(2, add_status(mission_list))
